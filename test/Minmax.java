140d42d2c93bcb4c8144a99fdfc9c8ed6ac69c72
Knium_ <passawee.jan@gmail.com>
2019-12-23 02:11:44 +0900

    Add test files.

---
+++ test/Minmax.java
@@ -0,0 +1,17 @@
+public class Minmax {
+    //  最大値の取得
+    public int max(int n1,int n2){
+        if (n1 > n2){
+            return n1;
+        }
+        return n2;
+    }
+    //  最大値の取得
+    public int min(int n1,int n2){
+        if (n1 < n2){
+            return n1;
+        }
+        return n2;
+    }
+ 
+}
